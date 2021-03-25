43255b751d59822bf010cf2ba6f238ce5888680e
Tom Wilson <twilson63@gmail.com>
2021-03-25 09:39:25 -0400

    completed

---
+++ api/movie-reviews/index.js
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+
+const url = process.env.HYPER
+const token = process.env.TOKEN
+
+const Review = z.object({
+  id: z.string().optional(),
+  title: z.string(),
+  body: z.string(),
+  rating: z.number().max(5)
+})
+
+export default async function (req, res) {
+  const { success, data, error } = Review.safeParse(req.body)
+  if (!success) { return res.status(500).json({ error: error.issues}) }
+
+  const result = await fetch(url + '/data/movie-reviews', { 
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(res => {
+    if (res.status !== 201) {
+      return ({ok: false, status: res.status, msg: 'error with service'})
+    }
+    return res.json()
+  }).catch(err => ({ok: false, status: 500, msg: err.message}))
+
+  res.status(result.status || 201).json(result)
+}
--- api/movie-reviews/index_test.js
+++ api/movie-reviews/index_test.js
@@ -1,6 +1,30 @@
 import { default as test } from 'tape'
+import postReviews from './index.js'
+import testServer from '@twilson63/test-server'
+import express from 'express'
+import fetch from 'node-fetch'
+import fetchMock from 'fetch-mock'
 
-test('ok', t => {
-  t.ok(true)
-  t.end()
+globalThis.fetch = fetchMock
+  .post('https://play.hyper63.com/data/movie-reviews', {
+    status: 201,
+    body: { ok: true }
+  })
+  .sandbox()
+
+const app = express()
+
+test('ok', async t => {
+  app.post('/api/movie-reviews', express.json(), postReviews)
+  const server = testServer(app)
+  const result = await fetch(server.url + '/api/movie-reviews', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ title: 'My First Review', body: '...', rating: 3})
+  }).then(res => res.json())
+
+  t.ok(result.ok)
+
+  server.close()
+  
 })
